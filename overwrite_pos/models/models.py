6ff066b044d435883772ccd76579be37e5b22bcc
Fortich <ajsuarezf@unal.edu.co>
2020-11-03 07:26:32 -0500

    Bringing testing changes (Custom POS)

--- overwrite_pos/models/models.py
+++ overwrite_pos/models/models.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from datetime import datetime
+from uuid import uuid4
+import pytz
 
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError, UserError
 
-# class overwrite_pos(models.Model):
-#     _name = 'overwrite_pos.overwrite_pos'
-#     _description = 'overwrite_pos.overwrite_pos'
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    numeracion_facturacion = fields.Char("Autorización Facturación")
+    range0 = fields.Char("Inicio rango autorizado")
+    range1 = fields.Char("Fin rango autorizado")
+    fact_code = fields.Char("Código consecutivo de factura")
